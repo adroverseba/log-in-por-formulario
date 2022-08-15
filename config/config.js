85142120e57724c0101f14287ad1daebe7490258
adroverseba <adroversebastian.sa@gmail.com>
2022-08-15 15:41:46 -0300

    Revert "agregar dotenv"
    This reverts commit 94a169cc5739d3d10a771f4cbcf12efcfed29296.

--- config/config.js
+++ config/config.js
@@ -1,13 +1,11 @@
-require("dotenv").config();
-
 const config = {
   env: process.env.NODE_ENV || "dev",
   port: process.env.PORT || 8080,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
+  dbUser: process.env.DB_USER || "postgres",
+  dbPassword: process.env.DB_PASSWORD || "battousaiseba",
+  dbHost: process.env.DB_HOST || "localhost",
+  dbName: process.env.DB_NAME || "eCommerce",
+  dbPort: process.env.DB_PORT || "5432",
 };
 
 module.exports = { config };
