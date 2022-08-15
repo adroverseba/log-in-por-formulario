94a169cc5739d3d10a771f4cbcf12efcfed29296
adroverseba <adroversebastian.sa@gmail.com>
2022-08-15 15:34:21 -0300

    agregar dotenv

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 const config = {
   env: process.env.NODE_ENV || "dev",
   port: process.env.PORT || 8080,
-  dbUser: process.env.DB_USER || "postgres",
-  dbPassword: process.env.DB_PASSWORD || "battousaiseba",
-  dbHost: process.env.DB_HOST || "localhost",
-  dbName: process.env.DB_NAME || "eCommerce",
-  dbPort: process.env.DB_PORT || "5432",
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  dbPort: process.env.DB_PORT,
 };
 
 module.exports = { config };
