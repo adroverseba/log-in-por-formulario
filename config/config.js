b5f931c4ca14f208c0c66e12d9a21fe0b49aaadc
adroverseba <adroversebastian.sa@gmail.com>
2022-09-06 16:46:25 -0300

    add conf heroku postgre

--- config/config.js
+++ config/config.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 
 const config = {
   env: process.env.NODE_ENV || "dev",
+  isProd: process.env.NODE_ENV === "production",
   port: process.env.PORT || 8080,
   dbUser: process.env.DB_USER || "postgres",
   dbPassword: process.env.DB_PASSWORD || "battousaiseba",
   dbHost: process.env.DB_HOST || "localhost",
   dbName: process.env.DB_NAME || "eCommerce",
   dbPort: process.env.DB_PORT || "5432",
+  dbUrl: process.env.DATABASE_URL,
 };
 
 module.exports = { config };
